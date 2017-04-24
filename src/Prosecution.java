cad6b8b2725129f560a97d091411d95687d97b9f
KB96 <kristiana_bailey@yahoo.com>
2017-04-24 15:08:50 -0400

    some buttons working

--- src/Prosecution.java
+++ src/Prosecution.java
@@ -8,11 +8,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Prosecution extends Application {
 
     public void start(Stage primaryStage) throws  Exception{
+        Text text1 = new Text("The charges brought against Mr. Stamos are grand theft and deflammation of character. Bob Saget Claims that John Stamos has been saying that Bob Saget always stole stuff off the set of full House such as a lamp, a dining room table set, and a fat stack of cash that belonged to the olson twins. As for the theft Saget claims that Stamos stole the show. ");
 
         Button choice1 = new Button("Restitution");
         Button choice2 = new Button("Jail");
@@ -30,19 +32,23 @@ public class Prosecution extends Application {
             }
         });*/
         Pane root = new Pane();
+        root.getChildren().add(text1);
+        text1.setLayoutX(10);
+        text1.setLayoutY(450);
+        text1.setWrappingWidth(600);
         root.getChildren().add(choice1);
-        choice1.setLayoutX(10);
+        choice1.setLayoutX(700);
         choice1.setLayoutY(470);
         root.getChildren().add(choice2);
-        choice2.setLayoutX(100);
+        choice2.setLayoutX(800);
         choice2.setLayoutY(470);
         root.getChildren().add(choice3);
-        choice3.setLayoutX(150);
+        choice3.setLayoutX(850);
         choice3.setLayoutY(470);
         root.getChildren().add(choice4);
-        choice4.setLayoutX(200);
+        choice4.setLayoutX(900);
         choice4.setLayoutY(470);
-        Image back = new Image("courtroom.jpg");
+        //Image back = new Image("courtroom.jpg");
         primaryStage.setTitle("Prosecution");
         primaryStage.setScene(new Scene(root, 1000,500));
         primaryStage.show();
